2013b3d46abda3d497b717846c55fa96ab71d38f
Lint Action <lint-action@samuelmeuli.com>
2021-07-12 14:32:12 +0000

    Fix code style issues with Prettier

--- src/ConstraintSystem.ts
+++ src/ConstraintSystem.ts
@@ -1,85 +1,87 @@
-import Constraint, {ConstraintData} from "./Constraint";
-import CSL from "./CSL";
-import ConstraintSystemPlugin from "./ConstraintSystemPlugin";
-
+import Constraint, {ConstraintData} from './Constraint';
+import CSL from './CSL';
+import ConstraintSystemPlugin from './ConstraintSystemPlugin';
 
 export interface Evaluation {
-    index?: number;
-    consistent: boolean;
-    message: string;
-    resource: any;
+  index?: number;
+  consistent: boolean;
+  message: string;
+  resource: any;
 }
 
 export default class ConstraintSystem<M, S> {
+  private readonly functions: {[key: string]: Function} = {};
+  private readonly constraints: Constraint<any, M, S>[] = [];
+  private readonly csl: CSL = new CSL();
 
-    private readonly functions: { [key: string]: Function } = {};
-    private readonly constraints: Constraint<any, M, S>[] = [];
-    private readonly csl: CSL = new CSL();
+  async registerPlugin(plugin: ConstraintSystemPlugin<M, S>) {
+    plugin
+      .registerFunctions(this)
+      .then(() =>
+        plugin
+          .registerConstraints(this)
+          .then(() =>
+            console.log('Registered plugin: ' + plugin.constructor.name)
+          )
+      );
+  }
 
-    async registerPlugin(plugin: ConstraintSystemPlugin<M, S>) {
-        plugin.registerFunctions(this).then(() =>
-            plugin.registerConstraints(this).then(() =>
-                console.log("Registered plugin: " + plugin.constructor.name)
-            )
-        );
+  addConstraint<T extends ConstraintData>(resource: T) {
+    this.constraints.push(new Constraint(resource, this.csl));
+  }
+
+  addFunction(name: string, fun: (...args: any[]) => any) {
+    this.addFun(name, fun);
+  }
+
+  addStatement(name: string, fun: (model: M, state: S) => any) {
+    this.addFun(name, fun);
+  }
+
+  private addFun(name: string, fun: Function) {
+    this.functions[name] = fun;
+    this.csl.registerFunction(name);
+  }
+
+  evaluate(model: M | M[], state: S): Evaluation[] {
+    if (model instanceof Array) {
+      return this.evaluateMultiple(model, state);
     }
+    return this.evaluateSingle(model, state);
+  }
 
-    addConstraint<T extends ConstraintData>(resource: T) {
-        this.constraints.push(new Constraint(resource, this.csl));
-    }
+  getMessage(msgString: string, model: M, state: S): string {
+    return this.csl.getMessage(msgString, model, state, this.functions);
+  }
 
-    addFunction(name: string, fun: (...args: any[]) => any) {
-        this.addFun(name, fun);
+  private evaluateMultiple(models: M[], state: S): Evaluation[] {
+    let res = [];
+    for (let i = 0; i < models.length; i++) {
+      let model = models[i];
+      let singleEvaluations = this.evaluateSingle(model, state);
+      for (let j = 0; j < singleEvaluations.length; j++) {
+        let singleEvaluation = singleEvaluations[i];
+        singleEvaluation.index = i;
+        res.push(singleEvaluation);
+      }
     }
+    return res;
+  }
 
-    addStatement(name: string, fun: (model: M, state: S) => any) {
-        this.addFun(name, fun);
+  private evaluateSingle(model: M, state: S): Evaluation[] {
+    let res = [];
+    let data = {
+      model: model,
+      state: state,
+      functions: this.functions,
+    };
+    for (let i = 0; i < this.constraints.length; i++) {
+      let constraint = this.constraints[i];
+      let evaluation = constraint.evaluate(data);
+      if (!evaluation.consistent) {
+        res.push(evaluation);
+      }
     }
-
-    private addFun(name: string, fun: Function) {
-        this.functions[name] = fun;
-        this.csl.registerFunction(name);
-    }
-
-    evaluate(model: M | M[], state: S): Evaluation[] {
-        if (model instanceof Array) {
-            return this.evaluateMultiple(model, state);
-        }
-        return this.evaluateSingle(model, state);
-    }
-
-    getMessage(msgString: string, model: M, state: S): string {
-        return this.csl.getMessage(msgString, model, state, this.functions);
-    }
-
-    private evaluateMultiple(models: M[], state: S): Evaluation[] {
-        let res = [];
-        for (let i = 0; i < models.length; i++) {
-            let model = models[i];
-            let singleEvaluations = this.evaluateSingle(model, state);
-            for (let j = 0; j < singleEvaluations.length; j++) {
-                let singleEvaluation = singleEvaluations[i];
-                singleEvaluation.index = i;
-                res.push(singleEvaluation);
-            }
-        }
-        return res;
-    }
-
-    private evaluateSingle(model: M, state: S): Evaluation[] {
-        let res = [];
-        let data = {
-            model: model,
-            state: state,
-            functions: this.functions
-        };
-        for (let i = 0; i < this.constraints.length; i++) {
-            let constraint = this.constraints[i];
-            let evaluation = constraint.evaluate(data);
-            if (!evaluation.consistent) {
-                res.push(evaluation);
-            }
-        }
-        return res;
-    }
+    return res;
+  }
 }
