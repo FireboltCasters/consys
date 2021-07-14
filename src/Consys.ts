3db0e4bda5dcaaa9be91e55a3ab9b3b7b77cc8d5
Nils <nilsbaumgartner1994@gmail.com>
2021-07-14 14:47:11 +0200

    Changed original author

--- src/Consys.ts
+++ src/Consys.ts
@@ -6,6 +6,13 @@
  * @class Consys
  */
 export default class Consys {
+
+  /**
+   * Creates a condition that allows only n simultaneous executions
+   * of a guarded function
+   * @param {number} number number that gets returned
+   * @returns {number} number which was given as input
+   */
   static exampleValue(number: number): number {
     return number;
   }
