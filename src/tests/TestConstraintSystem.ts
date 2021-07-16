7a26d5816225bf44b4e30b89cdf1013e3027c942
sdroppelmann <steffen.droppelmann@gmail.com>
2021-07-16 17:47:31 +0200

    better test coverage

--- src/tests/TestConstraintSystem.ts
+++ src/tests/TestConstraintSystem.ts
@@ -1,40 +1,58 @@
 import ConstraintSystem from "../ConstraintSystem";
 import ConstraintSystemPlugin from "../ConstraintSystemPlugin";
 
+type Model = {
+    time: string
+    maxLength: number
+};
+type State = {
+    currentTime: string
+};
+
+const model: Model = {
+    time: "5:00",
+    maxLength: 4
+};
+const state: State = {
+    currentTime: "7:00"
+}
 
 test('ConstraintSystem Test', async () => {
-    type Model = {
-        time: string
-        maxLength: number
-    };
-    type State = {
-        currentTime: string
-    };
     class Plugin extends ConstraintSystemPlugin<Model, State> {
         async registerConstraints(system: ConstraintSystem<Model, State>): Promise<void> {
             system.addConstraint({
                 assertion: "ALWAYS: LENGTH($time) && LENGTH(#currentTime) < $maxLength",
-                message: "failed"
+                message: "failed0"
+            });
+            system.addConstraint({
+                assertion: "ALWAYS: LENGTH($time) - LENGTH(#currentTime) == ZERO",
+                message: "failed1"
             });
         }
 
         async registerFunctions(system: ConstraintSystem<Model, State>): Promise<void> {
             system.addFunction("LENGTH", (string: string) => { return string.length });
+            system.addStatement("ZERO", () => { return 0 });
+            expect(() => {
+                system.addStatement("ZERO", () => { return 0 })
+            }).toThrowError();
+
+            const model: Model = {
+                time: "1:00",
+                maxLength: 10
+            };
+            const state: State = {
+                currentTime: "4:00"
+            }
+            expect(system.getMessage("ZERO", model, state)).toBe("0");
         }
     }
 
-    const model: Model = {
-        time: "5:00",
-        maxLength: 4
-    };
-    const state: State = {
-        currentTime: "7:00"
-    }
     const plugin = new Plugin();
     await plugin.init();
     const evaluation = plugin.evaluate(model, state);
     expect(evaluation.length).toBe(1);
     const instance = evaluation[0];
-    expect(instance.message).toEqual("failed");
+    expect(instance.message).toBe("failed0");
     expect(instance.consistent).toBe(false);
 });
