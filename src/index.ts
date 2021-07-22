1627265244803c9009521d2d7df0b54d92b4a1d7
sdroppelmann <steffen.droppelmann@gmail.com>
2021-07-22 12:18:32 +0200

    added export evaluation

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@
 import Constraint from './Constraint';
 import ConstraintSystem from './ConstraintSystem';
 import ConstraintSystemPlugin from './ConstraintSystemPlugin';
+import {Evaluation} from "./ConstraintSystem";
 
 export {Constraint};
 export {ConstraintSystem};
 export {ConstraintSystemPlugin as Plugin};
+export {Evaluation};
